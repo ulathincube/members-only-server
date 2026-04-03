724e179092a6236fcd8f53c8bfe466b066817c3a
Ulathi Ncube <lathincube@gmail.com>
2026-04-03 16:00:15 +0200

    Feat: setup cors config

--- src/app.ts
+++ src/app.ts
@@ -18,12 +18,14 @@ const PostgresSession = pgSession(session);
 
 const app = express();
 
-app.use(
-  cors({
-    origin: ['http://localhost:5173'],
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: ['http://localhost:5173'],
+//     credentials: true,
+//   }),
+// );
+
+app.use(cors());
 app.use(express.json());
 
 app.use(
